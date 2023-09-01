60819f356859c73e7bc1873735149f0d4c2cc677
PaolaaReyess <142684062+PaolaaReyess@users.noreply.github.com>
2023-08-31 20:13:49 -0500

    Add files via upload

---
+++ ejercicio 3.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int numA, numB;
+	    
+	    Scanner leer = new Scanner(System.in);
+	    System.out.println("Ingrese el primer número: ");
+	    numA = leer.nextInt();
+	    
+	    Scanner ler = new Scanner(System.in);
+	    System.out.println("Ingrese el segundo número: ");
+	    numB = ler.nextInt();
+	    
+	    if ( numA > 0 && numB >0)
+	        System.out.println("True");
+	}
+}
